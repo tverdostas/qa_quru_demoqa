cb7e2be26242820ed49cc3a80ac12dee8f84d118
Anastasiya Elkafrawi <atverdova78@gmail.com>
2025-06-18 23:43:06 +0400

    FormTest completed

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,50 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.baseUrl = "https://demoqa.com";
+    //    Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void testForm() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Practice Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("test1");
+        $("#lastName").setValue("test1");
+        $("#userEmail").setValue("test1@local.local");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("8963598710");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Social Studies").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("test_summer_copy.jpg"); // только для элементов с type = "file"
+        $("#currentAddress").setValue("test_address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Gurgaon")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive").shouldHave(text("test1"),
+                text("8963598710"));
+    }
+}
